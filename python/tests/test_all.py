5ee6ca16b0d2fc52b443190c7b390bcc8b35fe3f
wangmengdi <wangmengdi@smart-insight.com.cn>
2024-09-11 21:20:37 +0800

    更改包名称

--- python/tests/test_all.py
+++ python/tests/test_all.py
@@ -1,6 +1,6 @@
 import pytest
-import poly_match
+import poly_match_rs
 
 
 def test_sum_as_string():
-    assert poly_match.sum_as_string(1, 1) == "2"
+    assert poly_match_rs.sum_as_string(1, 1) == "2"
